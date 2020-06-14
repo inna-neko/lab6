88d1536f9d83d9aa4df672279a9ac953b0ef475a
inna-neko <palamarinna951@gmail.com>
2020-06-14 23:42:16 +0300

    iterator added

---
+++ DAL/Entities/Iterator/Iterator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Entities.Iterator
+{
+    public class Iterator<TValue>
+    {
+        private readonly IList<TValue> _list;
+
+        public bool HasNext => _list.Count < _position;
+
+        public TValue Value => _list[_position];
+
+        private int _position;
+
+        public Iterator(IList<TValue> list)
+        {
+            _list = list;
+        }
+
+        public void Next()
+        {
+            if (HasNext) throw new Exception();
+
+            _position++;
+        }
+    }
+}
